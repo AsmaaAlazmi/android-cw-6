66a2592217bc3935f8ef9f2ed5081db151414d65
AsmaaAlazmi056 <66563370+AsmaaAlazmi@users.noreply.github.com>
2020-07-01 17:55:32 +0300

    asmaa

---
+++ movie/app/src/main/java/com/example/movie/movie.java
@@ -0,0 +1,12 @@
+package com.example.movie;
+
+public class movie {
+
+    private String mainActor;
+    private String title;
+    private double movieRate ;
+    private int pgRate;
+    private String genre;
+
+
+}
